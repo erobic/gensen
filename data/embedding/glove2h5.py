cab1e453d26e6d2ccecf00963a4b2d70b6f7998a
erobic <robikshrestha@gmail.com>
2020-08-13 12:15:46 -0400

    Made it compatible for python 3

--- data/embedding/glove2h5.py
+++ data/embedding/glove2h5.py
@@ -2,6 +2,22 @@
 import h5py
 import numpy as np
 
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def tofloat(value):
+    if isfloat(value):
+        return float(value)
+    else:
+        return 0
+
+
 glove_vectors = [
     line.strip().split()
     for line in open('glove.840B.300d.txt', 'r')
@@ -9,8 +25,7 @@ glove_vectors = [
 
 vocab = [line[0] for line in glove_vectors]
 vectors = np.array(
-    [[float(val) for val in line[1:]] for line in glove_vectors]
-).astype(np.float32)
+    [[tofloat(val) for val in line[-300:]] for line in glove_vectors]).astype(np.float32)
 vocab = '\n'.join(vocab)
 
 f = h5py.File('glove.840B.300d.h5', 'w')
